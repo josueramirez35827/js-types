9ca2102e91d0d92a397a7255621c22a9bfa70925
Josue Ramirez-Romero <josueramirez35827@gmail.com>
2021-09-19 23:58:16 -0400

    hw week1 madlibs

---
+++ Madlibs/projects.js
@@ -0,0 +1,25 @@
+let player = prompt("Would you like Story One or Story Two?")
+
+let adjective = prompt("type an adjective");
+let generalplace = prompt("type a general place");
+let vehicle = prompt("type a vehicle name(plural)");
+let game = prompt("type the name of a game");
+let noun = prompt("type a plural noun");
+let verb = prompt("type an 'ing' verb");
+let anotherverb = prompt("type another 'ing' verb");
+let food = prompt("type a food name (plural)");
+let sport = prompt("type the name of a sport");
+let anotherverb1 = prompt("type another 'ing' verb");
+let emotion = prompt("type an emotion");
+let number = prompt("type a number");
+
+document.write("A vacation is when you take a trip to some " + adjective + " place near an amazing " + generalplace + "." +
+  " A good vacation place is one where you can ride " + vehicle + " or play " + game + " or go hunting for " + noun + "." +
+  " I like to spend my time " + verb + " or " + anotherverb + "." +
+  " When parents go on a vacation, they spend their time eating three " + food + " a day." +
+  " Usually, fathers play" + sport + ", and mothers spend their time " + anotherverb1 + "." +
+  " Adults need vacations more than kids because adults are always very" + emotion + " because they have to work " + number + " hours every day all year just to afford their vacations!"
+);
+
+
+
